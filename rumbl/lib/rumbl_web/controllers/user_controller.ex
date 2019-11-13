3eac4197337b98ca4efa4d2e06321ffe717c72c3
Paulo Silva <paulo.silva@plataformatec.com.br>
2019-11-13 15:05:22 -0300

    Implement user creation.

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -2,6 +2,23 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Accounts
+  alias Rumbl.Accounts.User
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%User{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    with {:ok, user} <- Accounts.create_user(user_params) do
+      conn
+      |> put_flash(:info, "#{user.name} created!")
+      |> redirect(to: Routes.user_path(conn, :index))
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
